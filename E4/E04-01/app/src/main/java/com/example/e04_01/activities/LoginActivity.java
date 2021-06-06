3b1a5742641cc2f84c6ce843dddab7d0820683f0
Armend Ukëhaxhaj <armend.ukehaxhaj@gmail.com>
2021-06-06 21:13:59 +0200

    add login activity

---
+++ E4/E04-01/app/src/main/java/com/example/e04_01/activities/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.e04_01.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e04_01.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+
+            // TODO: Start Content Activity
+        }
+    }
+}
