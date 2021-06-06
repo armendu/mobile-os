a1f9f9ac2686a2b51888d99103bdde068a8e18d0
Armend Ukëhaxhaj <armend.ukehaxhaj@gmail.com>
2021-06-06 15:31:00 +0200

    initial commit for e4

---
+++ E4/E03-01/app/src/main/java/com/example/e04_01/enums/Role.java
@@ -0,0 +1,7 @@
+package com.example.e04_01.enums;
+
+public enum Role {
+    HumanResources,
+    Management,
+    Technology
+}
---
+++ E4/E03-01/app/src/main/java/com/example/e04_01/viewmodels/Employee.java
@@ -0,0 +1,52 @@
+package com.example.e04_01.viewmodels;
+
+import com.example.e04_01.enums.Gender;
+import com.example.e04_01.enums.Role;
+
+public class Employee {
+    private String name;
+    private String biography;
+    private String imageUrl;
+    private Gender gender;
+    private Role role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
