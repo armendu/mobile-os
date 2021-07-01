1362e2caf9b237908ced3556a21802eb1b307d30
Armend Ukëhaxhaj <armend.ukehaxhaj@gmail.com>
2021-07-01 22:53:32 +0200

    add sample app for l12

---
+++ L12/Sample/Sample/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by Armend on 1.7.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Hello World")
+                Text("Hello World")
+                Text("Hello World")
+            }.navigationTitle("SAMPLE SWIFTUI")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
