67d6c335c3b672a1800bbf2cc5282f0979816685
Armend Ukëhaxhaj <armend.ukehaxhaj@gmail.com>
2023-05-20 23:19:55 +0200

    Add Firebase Authentication add initial firebase database add required dependencies rename mainactivity to LoginActivity

---
+++ 2023-SOPM/ClassStudentManagement/app/src/main/java/com/example/classstudentmanagement/LoginActivity.kt
@@ -0,0 +1,116 @@
+package com.example.classstudentmanagement
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "LoginActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        Log.d(TAG, "Called onCreate")
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        val button = findViewById<Button>(R.id.login_button)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        button.setOnClickListener {
+            if (email.text.isNullOrEmpty() || password.text.isNullOrEmpty()) {
+                Toast.makeText(this, "Username or Password is empty", Toast.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        showLongToastWithMessage("Logged in successfully. Hello ${auth.currentUser?.email}")
+                        val intent = Intent(this, RatingsActivity::class.java)
+                        intent.putExtra("USERNAME", email.text.toString())
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        showLongToastWithMessage("Authentication failed: ${task.exception}")
+                    }
+                }
+        }
+
+        val registerButton = findViewById<Button>(R.id.register_button)
+        registerButton.setOnClickListener {
+            createUserWith(email, password)
+        }
+    }
+
+    private fun showLongToastWithMessage(message: String) {
+        Toast.makeText(
+            this,
+            message,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun createUserWith(username: EditText, password: EditText) {
+        auth.createUserWithEmailAndPassword(username.text.toString(), password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    Log.d(TAG, "onCreate: Current User has the following: ${user?.email}")
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    showLongToastWithMessage("Authentication failed.")
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Called onStart")
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.d(TAG, "onStart: Current user is: ${currentUser.displayName}")
+            // reload()
+        } else {
+            Log.d(TAG, "onStart: User is not logged in")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Called onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Called onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Called onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Called onDestroy")
+    }
+}
--- 2023-SOPM/ClassStudentManagement/app/src/main/java/com/example/classstudentmanagement/RatingsActivity.kt
+++ 2023-SOPM/ClassStudentManagement/app/src/main/java/com/example/classstudentmanagement/RatingsActivity.kt
@@ -2,13 +2,46 @@ package com.example.classstudentmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.classstudentmanagement.databinding.ActivityRatingsBinding
 import com.example.classstudentmanagement.viewmodels.RatingsViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.database.ktx.values
+import com.google.firebase.ktx.Firebase
 
 class RatingsActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "RatingsActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Write a message to the database// Write a message to the database
+        val database = Firebase.database
+
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello World")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
 
         val binding: ActivityRatingsBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_ratings)
